e8a0b12eb761dd423fa33e77a4bec31fbc336f98
Альбина Мифтахова <almiftakhova@ozon.ru>
2022-07-15 03:29:05 +0300

    Initial commit

---
+++ AreaCalculation/Program.cs
@@ -0,0 +1,7 @@
+﻿using AreaCalculationLib;
+
+var firstUnknownFigure = FigureCreator.CreateFigure(3);
+Console.WriteLine(firstUnknownFigure.Area());
+            
+var secondUnknownFigure = FigureCreator.CreateFigure(3, 4, 5);
+Console.WriteLine(secondUnknownFigure.Area());
